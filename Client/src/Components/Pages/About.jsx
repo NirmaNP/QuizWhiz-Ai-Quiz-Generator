bf83f41c3f2f4ab9be4af1fd268b3837cdfe62b4
Nikhil-9876 <nikhilsolanki9876@gmail.com>
2025-06-15 19:37:03 +0530

    commenting the creators section

--- Client/src/Components/Pages/About.jsx
+++ Client/src/Components/Pages/About.jsx
@@ -140,7 +140,7 @@ function About() {
       </div>
 
       {/* Team Section */}
-      <div className="relative z-10 py-20 dark:bg-black">
+      {/* <div className="relative z-10 py-20 dark:bg-black">
         <div className="container mx-auto px-4">
           <div className="max-w-4xl mx-auto text-center mb-16">
             <h2 className="text-3xl md:text-4xl font-bold mb-4 text-gray-900 dark:text-white">
@@ -208,7 +208,7 @@ function About() {
             ))}
           </div>
         </div>
-      </div>
+      </div> */}
 
       {/* Tech Stack */}
       <div className="relative z-10 py-20 dark:bg-black backdrop-blur-lg">
